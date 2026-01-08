0acc8e6bf4961a8b4e2e3c4ba6d37e0be2d09315
Aleksandr Ershov <aleks.usbuz@gmail.com>
2026-01-07 17:45:35 -0800

    Added a new Title component and stored it all inside landing folder

--- app/(app)/contact/page.tsx
+++ app/(app)/contact/page.tsx
@@ -1,5 +1,3 @@
-import LandingLogo from "@/components/LandingLogo"
-
 export default function Contact() {
     return (
         <h1></h1>
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,4 +1,4 @@
-import LandingLogo from "@/components/LandingLogo"
+import LandingLogo from "@/components/landing/LandingLogo"
 
 export default function Home() {
     return (
