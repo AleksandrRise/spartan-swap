8770a9b52a4418a2497c767a821b8b4dcf24a0cd
Aleksandr Ershov <aleks.usbuz@gmail.com>
2026-01-08 18:51:57 -0800

    Added opacity to text to comply with figma design

--- src/components/landing/HeroSection.tsx
+++ src/components/landing/HeroSection.tsx
@@ -2,7 +2,7 @@ export default function HeroSection() {
 
   // Classes
   const wrapperClasses = "p-10"
-  const textWrapperClasses = "text-8xl leading-tight"
+  const textWrapperClasses = "text-8xl leading-tight opacity-87"
   const textNormalClasses = "font-light"
   const textBoldClasses = "font-bold"
   const btnClasses = "mt-8 bg-primary text-white px-9.5 py-4.5 text-2xl font-bold cursor-pointer"
