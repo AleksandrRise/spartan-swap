3c02a9d786e1923869429cc824c78e40cce7a670
Aleksandr Ershov <aleks.usbuz@gmail.com>
2026-01-14 17:09:56 -0800

    Added a navbar links list

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,12 +10,19 @@ export default function NavBar() {
     ];
 
     // Classes
+    const linksListClasses = ""
     const logoSrc = "/images/logo-white.png"
     const logoAlt = "Logo"
 
     return (
         <nav>
             <Image src={logoSrc} alt={logoAlt} width={22.5} height={22.5} />
+
+            <ul className={linksListClasses}>
+                {NAV.map((item) => (
+                    <li><a href={item.href}>{item.label}</a></li>
+                ))}
+            </ul>
         </nav>
     )
 }
