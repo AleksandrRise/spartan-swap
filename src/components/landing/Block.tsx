e4e6fda49dbf5d318d3aed30d384cac1cbbc0864
Aleksandr Ershov <aleks.usbuz@gmail.com>
2026-01-19 15:13:45 -0800

    Found a good size of sliding blocks, rotated by 15 degrees

--- src/components/landing/Block.tsx
+++ src/components/landing/Block.tsx
@@ -6,7 +6,7 @@ export default function Block({ label }: BlockType) {
 
     // Classes
     const blockClasses =
-        "bg-gray-100 border border-gray-200 rounded-lg px-4 py-10 text-center whitespace-pre-line";
+        "bg-gray-100 border border-gray-200 px-8 py-16 text-center whitespace-pre-line";
     const blockLabelClasses =
         "text-[11px] font-medium tracking-wide text-gray-400 uppercase";
 
