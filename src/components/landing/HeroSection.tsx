746d6ed5a99c36a82bc5cb6e338212a8fd9546eb
Aleksandr Ershov <aleks.usbuz@gmail.com>
2026-01-20 17:21:55 -0800

    Added fonts, fixed herosection

--- src/components/landing/HeroSection.tsx
+++ src/components/landing/HeroSection.tsx
@@ -2,10 +2,10 @@ export default function HeroSection() {
 
   // Classes
   const wrapperClasses = "py-10"
-  const textWrapperClasses = "text-8xl leading-tight opacity-87"
+  const textWrapperClasses = "text-8xl leading-tight opacity-87 font-display"
   const textNormalClasses = "font-light"
   const textBoldClasses = "font-bold"
-  const btnClasses = "mt-8 bg-primary text-white px-9.5 py-4.5 text-2xl font-bold cursor-pointer"
+  const btnClasses = "mt-8 bg-primary font-inter text-white px-9.5 py-4.5 text-2xl font-bold cursor-pointer"
 
   return (
     <section className={wrapperClasses}>
