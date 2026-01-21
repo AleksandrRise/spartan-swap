d0558c4fe71cd18b790c74494f6396393a7f6c4b
Aleksandr Ershov <aleks.usbuz@gmail.com>
2026-01-20 17:52:36 -0800

    Updated logo for landing page and added reference to a different page on button click

--- src/components/landing/LandingLogo.tsx
+++ src/components/landing/LandingLogo.tsx
@@ -5,7 +5,7 @@ export default function LandingLogo() {
 
     // Classes
     const titleLogo = "flex items-center gap-3"
-    const logoSrc = "/images/logo-white.png"
+    const logoSrc = "/images/spartan-logo.png"
     const logoText = "text-black text-5xl text-[#0A2E5C]"
     const logoSpartanText = "text-primary"
     const logoAlt = "Logo"
